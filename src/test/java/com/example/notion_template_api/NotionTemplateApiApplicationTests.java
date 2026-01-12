7dc92e036c83fc72f85160cbfb91c4520da66f8c
Sebastian <sebastianaugustolopes@gmail.com>
2026-01-12 10:40:47 -0300

    test: add basic application test
    Add basic Spring Boot application context test to verify application startup and configuration.

---
+++ src/test/java/com/example/notion_template_api/NotionTemplateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notion_template_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionTemplateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
